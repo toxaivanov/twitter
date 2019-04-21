ba4ea68d1de8b4d7b75daa17351583a96433fa52
toxaivanov <toxaivanov@me.com>
2019-04-21 12:49:53 +0300

    upd

--- normalization/stem.py
+++ normalization/stem.py
@@ -18,7 +18,7 @@ def remove_pattern(input_txt):
     return input_txt  
     
 train['tweet_text'] = np.vectorize(remove_pattern)(train['tweet_text'])
-train['tweet_text'] = train['tweet_text'].str.replace("[^a-zA-Z#]", " ")
+train['tweet_text'] = train['tweet_text'].str.replace("[^a-zA-Z]", " ")
 train['tweet_text'] = train['tweet_text'].apply(lambda x:' '.join(w for w in x.split() if len(w)>3))
 
 tokenized_tweet = train['tweet_text'].apply(lambda x: word_tokenize(x))
